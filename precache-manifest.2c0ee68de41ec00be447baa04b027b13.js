4f7a381d30cf26b33d05655826b89d744ea05cd7
kusokkulochok@gmail.com <kusokkulochok@gmail.com>
2020-02-14 22:53:20 +0200

    Updates

---
+++ precache-manifest.2c0ee68de41ec00be447baa04b027b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3b6d0ab34f05c275ce4a62f341aaf7",
+    "url": "/dbmovies/index.html"
+  },
+  {
+    "revision": "62318b2d1456c0ab7358",
+    "url": "/dbmovies/static/css/main.0103f39b.chunk.css"
+  },
+  {
+    "revision": "c6a923e74a511d252623",
+    "url": "/dbmovies/static/js/2.3cbce93b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/dbmovies/static/js/2.3cbce93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62318b2d1456c0ab7358",
+    "url": "/dbmovies/static/js/main.84d7e664.chunk.js"
+  },
+  {
+    "revision": "eeed7596a6ca1a85462a",
+    "url": "/dbmovies/static/js/runtime-main.9813c246.js"
+  }
+]);
